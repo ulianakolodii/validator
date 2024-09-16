3e79dfdf21b2883eff07733131af74d7178cb391
Uliana Kolodii <ulianakolodii@gmail.com>
2024-09-16 23:38:32 +0300

    feat: add tests for app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import App from "./App";
+import validatePassword from "./utils/validatePassword";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./utils/validatePassword", () => jest.fn());
+
+describe("App Component", () => {
+  it("should display valid password count after file is uploaded", async () => {
+    const mockFileContent = "a 1-5: jwjehrjkqa\nb 2-4: bbbbdjjdsld\nz 3-6: hjjiuyik";
+    const mockFile = new File([mockFileContent], "test.txt", { type: "text/plain" });
+
+    validatePassword.mockImplementation(() => true);
+
+    render(<App />);
+
+    const fileInput = screen.getByTestId("upload-input")
+
+    fireEvent.change(fileInput, { target: { files: [mockFile] } });
+
+    await waitFor(() => {
+      expect(screen.getByTestId('info-container').innerHTML).toBe('valid password: <strong>3</strong>');
+    });
+  });
 });
+
